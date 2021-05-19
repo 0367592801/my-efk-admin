ccf0e9fab2404e804d58a247f560dceba74d305e
minhpt <phantuanminhnh117@gmail.com>
2021-05-20 00:05:03 +0700

    buon ngu qua

--- src/views/LessonManage/LessonManage.js
+++ src/views/LessonManage/LessonManage.js
@@ -40,6 +40,7 @@ import {
 import Swal from "sweetalert2";
 import { apiUrl } from "../../ultil/apiUrl";
 import ReactPlayer from "react-player";
+import ModalEditLesson from "./_part/ModalEditLesson";
 
 const styles = {
   typo: {
@@ -78,15 +79,6 @@ const styles = {
   },
 };
 
-const formItemLayout = {
-  labelCol: {
-    span: 6,
-  },
-  wrapperCol: {
-    span: 14,
-  },
-};
-
 const useStyles = makeStyles(styles);
 
 export default function LessonManage() {
@@ -103,11 +95,6 @@ export default function LessonManage() {
   const [practiceVideo, setPracticeVideo] = useState(null);
   const [playVideo, setplayVideo] = useState(true);
 
-  // page
-  const [page, setPage] = useState(null);
-  const [visiblePageEdit, setVisiblePageEdit] = useState(false);
-  const [dataPage, setdataPage] = useState(null);
-
   const reactPlayer = useRef(null);
 
   async function fetchDataLesson() {
@@ -116,26 +103,11 @@ export default function LessonManage() {
     setdataLesson(res.data);
   }
 
-  async function fetchDataPage(lesson_id) {
-    let res = await getAllPage(lesson_id);
-    console.log(res);
-    setdataPage(res.data);
-  }
-
   useEffect(() => {
     fetchDataLesson();
   }, []);
-  const onChange = (pagination, filters, sorter, extra) => {
-    // console.log(extra.currentDataSource);
-    // this.setState({
-    //   selectedRowKeys: [],
-    //   loading: false,
-    //   current: Number(pagination.current),
-    //   pageSize: Number(pagination.pageSize),
-    // });
-  };
 
-  const onChangeTablePage = (pagination, filters, sorter, extra) => {
+  const onChange = (pagination, filters, sorter, extra) => {
     // console.log(extra.currentDataSource);
     // this.setState({
     //   selectedRowKeys: [],
@@ -203,23 +175,25 @@ export default function LessonManage() {
       });
       console.log("Received values of form: ", values);
 
-      // let data = {
-      //   season_name: values.season_name,
-      // };
-      // console.log(season);
-      // let res = season
-      //   ? await putSeason(season.id, data)
-      //   : await postSeason(data);
-      // console.log(res);
-      // let season_id = res.data.id;
+      let data = {
+        lesson_name: values.lesson_name,
+        description: values.description,
+        model_type: values.model_type,
+      };
+      console.log(lesson);
+      let res = lesson
+        ? await putLesson(lesson.id, data)
+        : await postLesson(data);
+      console.log(res);
+      let lesson_id = res.data.id;
 
-      // if (values.season_img)
-      //   await uploadMediaFile(
-      //     values.season_img[0],
-      //     "season",
-      //     season_id,
-      //     "season_img"
-      //   );
+      if (values.lesson_img)
+        await uploadMediaFile(
+          values.lesson_img[0],
+          "lesson",
+          lesson_id,
+          "lesson_img"
+        );
 
       setTimeout(() => {
         setVisibleEdit(false);
@@ -349,103 +323,6 @@ export default function LessonManage() {
     },
   ];
 
-  const columnsPages = [
-    {
-      title: "Page order",
-      dataIndex: "page_order",
-      key: "page_order",
-      // ...this.getColumnSearchProps("name"),
-      width: 200,
-    },
-    {
-      title: "Thumbnail",
-      dataIndex: "image_background",
-      key: "thumbnail",
-      align: "center",
-      render: (img) => (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Avatar
-            // onMouse
-            onClick={() => {
-              console.log(img);
-              Swal.fire({
-                imageUrl: `${apiUrl}${img?.url}`,
-                html: img && img?.url ? "" : "This season has no thumbnail!",
-                showConfirmButton: false,
-                showCloseButton: true,
-                customClass: {
-                  image: "storiesSwal",
-                },
-                width: 350,
-                padding: 0,
-              });
-            }}
-            src={`${apiUrl}${img?.url}`}
-          />
-        </div>
-      ),
-    },
-    {
-      title: "Action",
-      dataIndex: "id, page",
-      key: "action",
-      render: (id, page) => (
-        <div style={{ display: "flex" }}>
-          {/* {console.log('1,', season)} */}
-          <Fab
-            color="primary"
-            aria-label="add"
-            size="small"
-            onClick={() => {
-              console.log(page);
-              setPage(page);
-              setVisiblePageEdit(true);
-            }}
-            style={{ marginRight: "15px" }}
-          >
-            <Edit />
-          </Fab>
-          <Fab
-            color="secondary"
-            aria-label="delete"
-            size="small"
-            onClick={() => {
-              Swal.fire({
-                title: "Are you sure?",
-                text: "You won't be able to revert this!",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes, delete it!",
-              }).then(async (result) => {
-                if (result.isConfirmed) {
-                  try {
-                    await deletePage(page.id);
-                    Swal.fire(
-                      "Deleted!",
-                      "Your file has been deleted.",
-                      "success"
-                    );
-                    fetchDataPage();
-                  } catch (e) {
-                    Swal.fire({
-                      icon: "error",
-                      title: "Oops...",
-                      text: "Something went wrong, please try again!",
-                    });
-                  }
-                }
-              });
-            }}
-          >
-            <Delete />
-          </Fab>
-        </div>
-      ),
-    },
-  ];
-
   return (
     <Card>
       <CardHeader color="primary">
@@ -489,119 +366,15 @@ export default function LessonManage() {
             rowKey="id"
           />
         )}
-        <Modal
-          title="Lesson"
-          visible={visibleEdit}
-          footer={null}
-          onOk={handleOkEdit}
-          onCancel={handleCancelEdit}
-          width="800"
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-around",
-            }}
-          >
-            <Form
-              name="validate_other"
-              {...formItemLayout}
-              onFinish={onFinish}
-              form={form}
-            >
-              <Form.Item
-                name="lesson_name"
-                label="New name:"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name="description"
-                label="New desciption:"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name="model_type"
-                label="New model type name:"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name="lesson_video"
-                label={`${lesson != null ? "New Video:" : "Lesson video"}`}
-                valuePropName="fileList"
-                getValueFromEvent={normFile}
-                extra="lesson video"
-              >
-                <Upload
-                  id="video"
-                  name="logo"
-                  action="/upload.do"
-                  listType="video"
-                >
-                  <Button type="primary">
-                    <UploadOutlined />
-                    Click to upload lesson video
-                  </Button>
-                </Upload>
-              </Form.Item>
-              <Form.Item
-                wrapperCol={{
-                  span: 12,
-                  offset: 6,
-                }}
-              >
-                <Button type="info" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-            {lesson && (
-              <Table
-                // ref={this.tableRef}
-                dataSource={lesson.pages}
-                style={{
-                  padding: "5px",
-                  borderRadius: "5px",
-                  backgroundColor: "white",
-                  // paddingBottom: `${this.state.levelHeight}px`,
-                  // paddingTop: dataAllStories.length > 0 ? '0px' : '0px',
-                  // clear: 'both',
-                }}
-                columns={columnsPages}
-                onChange={onChangeTablePage()}
-                pagination={{
-                  // pageSize: 5,
-                  pageSizeOptions: ["5", "10", "20"],
-                  showSizeChanger: true,
-                  locale: { items_per_page: "" },
-                  defaultPageSize: 5,
-                  // onShowSizeChange={this.onShowSizeChange}
-                }}
-                // locale={{ filterReset: '' }}
-                // rowSelection={rowSelection}
-                rowKey="id"
-              />
-            )}
-          </div>
-        </Modal>
+        <ModalEditLesson
+          visibleEdit={visibleEdit}
+          handleOkEdit={handleOkEdit}
+          handleCancelEdit={handleCancelEdit}
+          onFinish={onFinish}
+          form={form}
+          lesson={lesson}
+          normFile={normFile}
+        />
         <Modal
           title="Practice Video"
           visible={visibleVideo}
--- src/views/LessonManage/_part/ModalEditLesson.jsx
+++ src/views/LessonManage/_part/ModalEditLesson.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Table,
   Divider,
@@ -12,52 +11,182 @@ import {
   Form,
   Button,
 } from "antd";
+import {
+  Edit,
+  Delete,
+  Add,
+  CollectionsBookmarkOutlined,
+} from "@material-ui/icons";
+import { Grid, Chip, Avatar, Fab } from "@material-ui/core";
+import {
+  getAllLesson,
+  getLesson,
+  postLesson,
+  uploadMediaFile,
+  putLesson,
+  deleteLesson,
+  deletePage,
+  getAllPage,
+} from "../../../ultil/api";
 import "antd/dist/antd.css";
 import Swal from "sweetalert2";
-import { apiUrl } from "../../ultil/apiUrl";
+import { apiUrl } from "../../../ultil/apiUrl";
+import { UploadOutlined } from "@ant-design/icons";
 
-class ModalEditLesson extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const formItemLayout = {
+  labelCol: {
+    span: 6,
+  },
+  wrapperCol: {
+    span: 14,
+  },
+};
+
+export default function ModalEditLesson(props) {
+  console.log(props);
+  // page
+  const [page, setPage] = useState(null);
+  const [visiblePageEdit, setVisiblePageEdit] = useState(false);
+  const [dataPage, setdataPage] = useState(null);
+
+  async function fetchDataPage(lesson_id) {
+    let res = await getAllPage(lesson_id);
+    console.log(res);
+    setdataPage(res.data);
   }
-  render() {
-    return (
-      <Modal
-        title="Lesson"
-        visible={visibleEdit}
-        footer={null}
-        onOk={handleOkEdit}
-        onCancel={handleCancelEdit}
-        width="800"
+
+  const columnsPages = [
+    {
+      title: "Page order",
+      dataIndex: "page_order",
+      key: "page_order",
+      // ...this.getColumnSearchProps("name"),
+      width: 200,
+    },
+    {
+      title: "Thumbnail",
+      dataIndex: "image_background",
+      key: "thumbnail",
+      align: "center",
+      render: (img) => (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Avatar
+            // onMouse
+            onClick={() => {
+              console.log(img);
+              Swal.fire({
+                imageUrl: `${apiUrl}${img?.url}`,
+                html: img && img?.url ? "" : "This season has no thumbnail!",
+                showConfirmButton: false,
+                showCloseButton: true,
+                customClass: {
+                  image: "storiesSwal",
+                },
+                width: 350,
+                padding: 0,
+              });
+            }}
+            src={`${apiUrl}${img?.url}`}
+          />
+        </div>
+      ),
+    },
+    {
+      title: "Action",
+      dataIndex: "id, page",
+      key: "action",
+      render: (id, page) => (
+        <div style={{ display: "flex" }}>
+          {/* {console.log('1,', season)} */}
+          <Fab
+            color="primary"
+            aria-label="add"
+            size="small"
+            onClick={() => {
+              console.log(page);
+              setPage(page);
+              setVisiblePageEdit(true);
+            }}
+            style={{ marginRight: "15px" }}
+          >
+            <Edit />
+          </Fab>
+          <Fab
+            color="secondary"
+            aria-label="delete"
+            size="small"
+            onClick={() => {
+              Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!",
+              }).then(async (result) => {
+                if (result.isConfirmed) {
+                  try {
+                    await deletePage(page.id);
+                    Swal.fire(
+                      "Deleted!",
+                      "Your file has been deleted.",
+                      "success"
+                    );
+                    fetchDataPage();
+                  } catch (e) {
+                    Swal.fire({
+                      icon: "error",
+                      title: "Oops...",
+                      text: "Something went wrong, please try again!",
+                    });
+                  }
+                }
+              });
+            }}
+          >
+            <Delete />
+          </Fab>
+        </div>
+      ),
+    },
+  ];
+
+  const onChangeTablePage = (pagination, filters, sorter, extra) => {
+    // console.log(extra.currentDataSource);
+    // this.setState({
+    //   selectedRowKeys: [],
+    //   loading: false,
+    //   current: Number(pagination.current),
+    //   pageSize: Number(pagination.pageSize),
+    // });
+  };
+
+  return (
+    <Modal
+      title="Lesson"
+      visible={props.visibleEdit}
+      footer={null}
+      onOk={props.handleOkEdit}
+      onCancel={props.handleCancelEdit}
+      width="800"
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+        }}
       >
         <Form
           name="validate_other"
           {...formItemLayout}
-          onFinish={onFinish}
-          form={form}
+          onFinish={props.onFinish}
+          form={props.form}
         >
-          {lesson != null && (
-            <Form.Item label="Old name: ">
-              {/* <span>{season.season_name}</span> */}
-            </Form.Item>
-          )}
-          {/* {lesson != null && (
-                <Form.Item label="Old thumbnail: ">
-                  {lesson.season_img ? (
-                    <img
-                      style={{ margin: "0 auto" }}
-                      src={`${apiUrl}${season.season_img?.url}`}
-                      width="100px"
-                    />
-                  ) : (
-                    "This season has no thumbnail!"
-                  )}
-                </Form.Item>
-              )} */}
           <Form.Item
             name="lesson_name"
-            label={`${lesson != null ? "New name:" : "Lesson name"}`}
+            label="New name:"
             rules={[
               {
                 required: true,
@@ -66,28 +195,42 @@ class ModalEditLesson extends Component {
           >
             <Input />
           </Form.Item>
-
-          {/* <Form.Item
-                name="season_img"
-                label={`${
-                  season != null ? "New thumbnail:" : "Season thumbnail"
-                }`}
-                valuePropName="fileList"
-                getValueFromEvent={normFile}
-                extra="season thumbnail"
-              >
-                <Upload
-                  id="thumbnail"
-                  name="logo"
-                  action="/upload.do"
-                  listType="picture"
-                >
-                  <Button type="primary">
-                    <UploadOutlined />
-                    Click to upload season thumbnail
-                  </Button>
-                </Upload>
-              </Form.Item> */}
+          <Form.Item
+            name="description"
+            label="New desciption:"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="model_type"
+            label="New model type name:"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="lesson_video"
+            label={`${props.lesson != null ? "New Video:" : "Lesson video"}`}
+            valuePropName="fileList"
+            getValueFromEvent={props.normFile}
+            extra="lesson video"
+          >
+            <Upload id="video" name="logo" action="/upload.do" listType="video">
+              <Button type="primary">
+                <UploadOutlined />
+                Click to upload lesson video
+              </Button>
+            </Upload>
+          </Form.Item>
           <Form.Item
             wrapperCol={{
               span: 12,
@@ -99,9 +242,34 @@ class ModalEditLesson extends Component {
             </Button>
           </Form.Item>
         </Form>
-      </Modal>
-    );
-  }
+        {props.lesson && (
+          <Table
+            // ref={this.tableRef}
+            dataSource={props.lesson.pages}
+            style={{
+              padding: "5px",
+              borderRadius: "5px",
+              backgroundColor: "white",
+              // paddingBottom: `${this.state.levelHeight}px`,
+              // paddingTop: dataAllStories.length > 0 ? '0px' : '0px',
+              // clear: 'both',
+            }}
+            columns={columnsPages}
+            onChange={onChangeTablePage()}
+            pagination={{
+              // pageSize: 5,
+              pageSizeOptions: ["5", "10", "20"],
+              showSizeChanger: true,
+              locale: { items_per_page: "" },
+              defaultPageSize: 5,
+              // onShowSizeChange={this.onShowSizeChange}
+            }}
+            // locale={{ filterReset: '' }}
+            // rowSelection={rowSelection}
+            rowKey="id"
+          />
+        )}
+      </div>
+    </Modal>
+  );
 }
-
-export default withRouter(ModalEditLesson);
